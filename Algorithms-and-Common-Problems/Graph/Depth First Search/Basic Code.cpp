b7714c10770f6b98ff61e8b08a0c8c31eee7e60d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2024-08-24 12:24:23 -0700

    Update Basic Code.cpp

--- Algorithms-and-Common-Problems/Graph/Depth First Search/Basic Code.cpp
+++ Algorithms-and-Common-Problems/Graph/Depth First Search/Basic Code.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 typedef long long int ll;
 
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
 const ll N = 1e5 + 7;
 vector<ll> adjList[N];
 bool visited[N];
