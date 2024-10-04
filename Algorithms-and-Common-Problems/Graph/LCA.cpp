e475d7e2c5e2facd1d1855fb966ec6d1c87f6600
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2024-10-04 01:56:43 -0700

    Update LCA.cpp

--- Algorithms-and-Common-Problems/Graph/LCA.cpp
+++ Algorithms-and-Common-Problems/Graph/LCA.cpp
@@ -1,65 +1,63 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-typedef long long int ll;
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
-const ll N = 1e7+7;
-ll depth[N], parent[N][20];
+const ll N = 2e5+7, M = 20;
 vector<ll> adjList[N];
+ll par[N][M+1], depth[N], sz[N];
 
-void dfs(ll curr, ll par) {
-    depth[curr] = depth[par] + 1;
-    parent[curr][0] = par;
-    for (auto &u : adjList[curr]) if (u != par) dfs(u, curr);    
-}
-
-void preCompute(ll n) {
-    for (ll i = 1; i < 20; i++) {
-        for (ll node = 1; node <= n; node++) {
-            if (parent[node][i-1] != -1) {
-                parent[node][i] = parent[parent[node][i-1]][i-1];
-            }
-        }
-    }
+void dfs(ll u, ll p = 0) {
+	par[u][0] = p;
+	depth[u] = depth[p] + 1;
+	sz[u] = 1;
+	for (ll i = 1; i <= M; i++) par[u][i] = par[par[u][i - 1]][i - 1];
+	for (auto v: adjList[u]) if (v != p) {
+		dfs(v, u);
+		sz[u] += sz[v];
+	}
 }
 
 ll lca(ll u, ll v) {
-    if (depth[v] < depth[u]) swap(u, v);
-    ll diff = depth[v] - depth[u];
-    for (ll i = 0; i < 20; i++) {
-        if ((diff >> i) & 1) v = parent[v][i];
-    }
-
-    if (u == v) return u;
-
-    for (ll i = 19; i >= 0; i--) {
-        if (parent[u][i] != parent[v][i]) {
-            u = parent[u][i];
-            v = parent[v][i];
-        }
-    }
-    return parent[u][0];
+	if (depth[u] < depth[v]) swap(u, v);
+	for (ll k = M; k >= 0; k--) if (depth[par[u][k]] >= depth[v]) u = par[u][k];
+	if (u == v) return u;
+	for (ll k = M; k >= 0; k--) if (par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
+	return par[u][0];
 }
 
-int main()
-{
-    optimize();
-    ll n, q; cin >> n >> q;
+ll kth(ll u, ll k) {
+	assert(k >= 0);
+	for (ll i = 0; i <= M; i++) if (k & (1 << i)) u = par[u][i];
+	return u;
+}
 
-    for (ll i = 1; i < n; i++) {
+ll dist(ll u, ll v) {
+	ll l = lca(u, v);
+	return depth[u] + depth[v] - (depth[l] << 1);
+}
+
+//kth node from u to v, 0th node is u
+ll go(ll u, ll v, ll k) {
+	ll l = lca(u, v);
+	ll d = depth[u] + depth[v] - (depth[l] << 1);
+	assert(k <= d);
+	if (depth[l] + k <= depth[u]) return kth(u, k);
+	k -= depth[u] - depth[l];
+	return kth(v, depth[v] - depth[l] - k);
+}
+
+signed main() {
+	ll n; cin >> n;
+	for (ll i = 1; i < n; i++) {
         ll u, v; cin >> u >> v;
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
-    }
-
-    depth[1] = 0;
-    dfs(1, 1);
-    preCompute(n);
-
-    while (q--) {
-        ll a, b; cin >> a >> b;
-        ll lcaNode = lca(a, b);
-        cout << lcaNode << endl;
-    }
+	    adjList[u].push_back(v);
+	    adjList[v].push_back(u);
+	}
+	
+	dfs(1);
+	ll q; cin >> q;
+	while (q--) {
+	    ll u, v; cin >> u >> v;
+	    cout << dist(u, v) << '\n';
+	}
 }
