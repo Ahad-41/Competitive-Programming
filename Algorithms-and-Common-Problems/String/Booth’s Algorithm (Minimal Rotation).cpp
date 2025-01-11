9040bdd32b1d22c85aed86692c462d7300d6c71d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-01-11 21:32:57 +0600

    Create Booth’s Algorithm (Minimal Rotation).cpp

---
+++ Algorithms-and-Common-Problems/String/Booth’s Algorithm (Minimal Rotation).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll booths(string s) {
+    s += s;
+    vector<ll> failure(s.size(), -1);
+    ll mnRotationIndx = 0;
+
+    for (ll i = 1; i < s.size(); i++) {
+        char currentChar = s[i];
+        ll failureIdx = failure[i-mnRotationIndx-1];
+
+        while (failureIdx != -1 and currentChar != s[mnRotationIndx + failureIdx + 1]) {
+            if (currentChar < s[mnRotationIndx + failureIdx + 1]) mnRotationIndx = i-failureIdx-1;
+            failureIdx = failure[failureIdx];
+        }
+
+        if (currentChar != s[mnRotationIndx+failureIdx+1]) {
+            if (currentChar < s[mnRotationIndx]) mnRotationIndx = i;
+            failure[i-mnRotationIndx] = -1;
+        }
+        else failure[i - mnRotationIndx] = failureIdx + 1;    
+    }
+
+    return mnRotationIndx;
+}
+
+signed main()
+{
+    string s; cin >> s;
+    ll i = booths(s);
+    cout << s.substr(i) + s.substr(0, i);
+}
