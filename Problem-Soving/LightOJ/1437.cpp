7bf4fa07f0f046ade5f45044df37c6860d28558d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-01-17 09:52:50 +0600

    Create 1437.cpp

---
+++ Problem-Soving/LightOJ/1437.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll N = 505;
+vector <ll> adjList[N];
+ll n, m, parent[N], cost[N];
+
+ll bfs (ll rootNode) {
+    for (ll i = 0; i < n; i++) {
+        parent[i] = -1;
+        cost[i] = 1e18;
+    }
+   
+    queue <ll> q;
+    q.push(rootNode);
+    cost[rootNode] = 0;
+
+    ll mnCycle = 1e18;
+    while (!q.empty()) {
+        ll currNode = q.front();
+        q.pop();
+
+        for (auto &u : adjList[currNode]) {
+            if (cost[u] == 1e18) {
+                cost[u] = cost[currNode]+1;
+                parent[u] = currNode;
+                q.push(u);
+            }
+            else if (parent[currNode] != u) mnCycle = min(mnCycle, cost[currNode]+cost[u]+1);
+        }
+    }
+
+    return mnCycle;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int tc, tt = 1; cin >> tc;
+
+    test:
+    while (tc--) {
+        cout << "Case " << tt++ << ": ";
+        cin >> n >> m;
+        
+        for (ll i = 0; i < n; i++) adjList[i].clear();
+        while (m--) {
+            ll u, v; cin >> u >> v;
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        
+        ll ans = 1e18;
+        for (ll i = 0; i < n; i++) {
+            ans = min(ans, bfs(i));
+        }
+
+        if (ans == 1e18) cout << "impossible\n";
+        else cout << ans << "\n";
+    }
+}
