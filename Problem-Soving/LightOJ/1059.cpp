a68fc63be26ae42d48a6d6f1a63873eed849feef
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-01-19 00:47:03 +0600

    Create 1059.cpp

---
+++ Problem-Soving/LightOJ/1059.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll N = 1e4 + 7;
+vector<pair<ll, ll>> adjList[N];
+bool alreadyTaken[N];
+
+ll mst(ll currNode) {
+    priority_queue<pair<ll, ll>> pq;
+    pq.push({0, currNode});
+    ll minCost = 0;
+
+    while (!pq.empty()) {
+        ll cost = -pq.top().first;
+        currNode = pq.top().second;
+        pq.pop();
+        if (alreadyTaken[currNode] == true) continue;
+        
+        minCost += cost;
+        alreadyTaken[currNode] = true;
+        
+        for (auto &u : adjList[currNode]) {
+            if (alreadyTaken[u.first] == false) {
+                pq.push({-u.second, u.first});
+            }
+        }         
+    } 
+
+    return minCost;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int tc, tt = 1; cin >> tc;
+
+    test:
+    while (tc--) {
+        cout << "Case " << tt++ << ": ";
+        
+        ll n, m, a; cin >> n >> m >> a;
+        for (ll i = 1; i <= n; i++) {
+            adjList[i].clear();
+            alreadyTaken[i] = false;
+        }
+
+        while (m--) {
+            ll u, v, w; cin >> u >> v >> w;
+            if (w < a) {
+                adjList[u].push_back({v, w});
+                adjList[v].push_back({u, w});
+            }
+        }
+
+        ll ans = 0, cc = 0;
+        for (ll i = 1; i <= n; i++) {
+            if (alreadyTaken[i]) continue;
+            ll curr = 
+            ans += mst(i);
+            cc++;
+        }
+
+        cout << ans + cc*a << " " << cc << "\n";
+    }
+}
