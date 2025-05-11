90e96b17c94653212ab07ca9627b90006bb3d53c
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-05-11 09:46:35 +0600

    Create H. Litmus Test.cpp

---
+++ Problem-Soving/IUPC/DUET IUPC 2025/H. Litmus Test.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+signed main() {
+    ios_base::sync_with_stdio(); cin.tie();
+    ll tt = 1; 
+    cin >> tt;
+
+    test:
+    while (tt--) {
+        ll n; cin >> n;
+        string ss; cin >> ss;
+        
+        ll s = 0, a = 0, b = 0;
+        for (auto &u : ss) {
+            if (u == 'S') s++;
+            else if (u == 'B') b++;
+            else a++;
+        }
+
+        if (a == n or b == n) cout << n << "\n";
+        else if (a and b) cout << "1\n";
+        else if (a) cout << a << "\n";
+        else if (b) cout << b << "\n";
+        else cout << "2\n";
+    }
+}
