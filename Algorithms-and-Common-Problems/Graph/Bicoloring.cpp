319cb157b87003c2ee1766865485cc3e39185bde
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-08-24 23:10:42 +0600

    Create Bicoloring.cpp

---
+++ Algorithms-and-Common-Problems/Graph/Bicoloring.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ 
+const ll N = 1e5+7;
+vector<ll> adjList[N];
+ll visited[N], color[N];
+ 
+signed main() {
+    ll n, edge; cin >> n >> edge;
+    
+    while (edge--) {
+        ll u, v; cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+ 
+    for (ll i = 1; i <= n; i++) {
+        if (visited[i]) continue;
+        queue<ll> q;
+        q.push(i);
+        visited[i] = color[i] = 1;
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            
+            for (auto &u : adjList[it]) {
+                if (visited[u] and color[u] == color[it]) {
+                    cout << "IMPOSSIBLE";
+                    return 0;
+                }
+                if (visited[u]) continue;
+                q.push(u);
+                visited[u] = 1;
+                color[u] = (color[it] == 1 ? 2 : 1);
+            }
+        }
+    }
+ 
+    for (ll i = 1; i <= n; i++) cout << color[i] << " ";
+}
