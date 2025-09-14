ad059a3af45a866e1b3f39db2569445cc9d7c161
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 19:36:14 +0600

    Update Longest Substring Without Repeating Characters.cpp

--- Algorithms-and-Common-Problems/Longest Substring Without Repeating Characters.cpp
+++ Algorithms-and-Common-Problems/Longest Substring Without Repeating Characters.cpp
@@ -1,15 +1,12 @@
-int lengthOfLongestSubstring(string s) {
-        unordered_map <char, int> cnt;
-        
-        int start = 0, mxLength = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(cnt.find(s[i]) != cnt.end() && cnt[s[i]] >= start){
-                start = cnt[s[i]] + 1;
-            }
-            
-            cnt[s[i]] = i;
-            mxLength = max(mxLength, i-start+1);
-        }
-        
-        return mxLength;
+ll lengthOfLongestSubstring(string s) {
+    unordered_map <char, ll> cnt;
+    
+    ll start = 0, mxLength = 0;
+    for (ll i = 0; i < s.size(); i++){
+        if (cnt.find(s[i]) != cnt.end() and cnt[s[i]] >= start) start = cnt[s[i]]+1;
+        cnt[s[i]] = i;
+        mxLength = max(mxLength, i-start+1);
+    }
+    
+    return mxLength;
 }
