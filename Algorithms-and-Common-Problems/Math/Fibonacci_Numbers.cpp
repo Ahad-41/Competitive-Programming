3ccbe0a88d67b5158dc689caba75555fa9e6cd9d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 18:24:30 +0600

    Update Fibonacci_Numbers.cpp

--- Algorithms-and-Common-Problems/Math/Fibonacci_Numbers.cpp
+++ Algorithms-and-Common-Problems/Math/Fibonacci_Numbers.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+typedef long long ll;
 
 const ll M = 1e9+7;
 map<ll, ll> fib;
@@ -8,23 +8,23 @@ map<ll, ll> fib;
 ll func(ll n) {
     if (fib.find(n) != fib.end()) return fib[n];
     ll k = n / 2;
-	
+    
     if (n % 2 == 0) { // n = 2*k
-        return fib[n] = (func(k) * func(k) + func(k-1) * func(k-1)) % M;
+        ll a = func(k);
+        ll b = func(k-1);
+        return fib[n] = (a*a + b*b) % M;
     } 
     else { // n = 2*k+1
-	return fib[n] = (func(k)*func(k+1) + func(k-1)*func(k)) % M;
+        ll a = func(k);
+        return fib[n] = (a*func(k+1) + func(k-1)*a) % M;
     }
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
     ll n; cin >> n;
 
     fib[0] = fib[1] = 1;
-    if (n == 0) cout << 0 << endl;
-    else cout << func(n-1) << endl;
-
-    return 0;
+    if (n == 0) cout << "0\n";
+    else cout << func(n-1) << "\n";
 }
