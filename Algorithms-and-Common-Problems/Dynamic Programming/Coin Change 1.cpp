1c0ea4373bc4b04aa905c0a8009a0ee238cc3870
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 17:39:41 +0600

    Update Coin Change 1.cpp

--- Algorithms-and-Common-Problems/Dynamic Programming/Coin Change 1.cpp
+++ Algorithms-and-Common-Problems/Dynamic Programming/Coin Change 1.cpp
@@ -2,12 +2,8 @@ ll func(ll amount) {
     if (amount == 0) return 0;
     if (dp[amount] != -1) return dp[amount];
 
-    ll ans = INT_MAX;
-    for (auto &u : coins) {
-        if (amount - u >= 0) {
-            ans = min(ans + 0LL, func(amount - u, coins) + 1LL);
-        }
-    }
+    ll ans = 1e18;
+    for (auto &u : coins) if (amount - u >= 0) ans = min(ans, func(amount-u)+1);
 
     return dp[amount] = ans; 
 }
