144fba9bef58b15b2067bffe81312c356d10b222
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 23:22:40 +0600

    Update Range Minimum Query (RMQ).cpp

--- Algorithms-and-Common-Problems/Data Structure/Range Minimum Query (RMQ).cpp
+++ Algorithms-and-Common-Problems/Data Structure/Range Minimum Query (RMQ).cpp
@@ -1,16 +1,16 @@
-const ll N = 1e6 + 7;
+const ll N = 1e6+7;
 const ll LG = 22;
 ll arr[N], t[N][LG];
-void build(ll m) {
-    for (ll i = 1; i <= m; i++) t[i][0] = arr[i];
+void build(ll n) {
+    for (ll i = 1; i <= n; i++) t[i][0] = arr[i];
     for (ll k = 1; k < LG; k++) {
-        for (ll i = 1; i + (1 << k) - 1 <= m; i++) {
-            t[i][k] = min(t[i][k - 1], t[i + (1 << (k - 1))][k - 1]);
+        for (ll i = 1; i + (1LL << k) - 1 <= n; i++) {
+            t[i][k] = min(t[i][k-1], t[i+(1 << (k-1))][k-1]);
         }
     }
 }
  
 ll query(ll l, ll r) {
-    ll k = 31 - __builtin_clz(r - l + 1);
-    return min(t[l][k], t[r - (1 << k) + 1][k]);
+    ll k = 31 - __builtin_clz(r-l+1);
+    return min(t[l][k], t[r-(1LL << k)+1][k]);
 }
