e697371a2477200c7540e0875a8e2b82d4417ef1
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 17:35:32 +0600

    Update and rename Finding Bridges.cpp to Bridges Finding.cpp

--- Algorithms-and-Common-Problems/Graph/Bridges Finding.cpp
+++ Algorithms-and-Common-Problems/Graph/Bridges Finding.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+typedef long long ll;
 
-const ll N = 1e5+5;
+const ll N = 2e5+7;
 ll n, m, timer, disTime[N], low[N], bridgeCount;
 vector<ll> adjList[N];
 bool visited[N];
 
-void dfs(ll currNode, ll parent) {
+void dfs(ll currNode, ll par) {
     visited[currNode] = true;
     disTime[currNode] = low[currNode] = timer++;
     
     for (auto &u : adjList[currNode]) {
-        if (u == parent) continue;
+        if (u == par) continue;
         if (visited[u]) low[currNode] = min(low[currNode], disTime[u]);
         else {
             dfs(u, currNode);
@@ -22,12 +22,14 @@ void dfs(ll currNode, ll parent) {
     }
 }
 
-int main()
-{
-    ll tt; cin >> tt;
-    for (ll t = 1; t <= tt; t++) {
-        
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll tc; cin >> tc;
+
+    test:
+    while (tc--) {
         cin >> n >> m;
+
         timer = bridgeCount = 0;
         for (ll i = 1; i <= n; i++) {
             adjList[i].clear();
@@ -41,10 +43,7 @@ int main()
             adjList[v].push_back(u);
         }
 
-        for (ll i = 1; i <= n; i++) {
-            if (visited[i] == false) dfs(i, -1);
-        }
-
-        cout <<  bridgeCount << endl;
+        for (ll i = 1; i <= n; i++) if (visited[i] == false) dfs(i, -1);
+        cout <<  bridgeCount << "\n";
     }
 }
