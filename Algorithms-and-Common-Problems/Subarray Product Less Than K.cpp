d2243e9181557906fbfe291ae4d4d3ad18a04f5d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:34:24 +0600

    Update Subarray Product Less Than K.cpp

--- Algorithms-and-Common-Problems/Subarray Product Less Than K.cpp
+++ Algorithms-and-Common-Problems/Subarray Product Less Than K.cpp
@@ -1,14 +1,12 @@
-int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if(k <= 1) return 0;
-        
-        int subArray = 0, product = 1, start = 0;
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++){
-            product *= nums[i];
-            while(product >= k and start < n) product /= nums[start++];
-            subArray += i - start + 1;
-        }
-        
-        return subArray;
+ll numSubarrayProductLessThanK(vector<ll>& nums, ll k) {
+    if (k <= 1) return 0;
+    
+    ll n = nums.size(), subArray = 0, product = 1, start = 0;
+    for (ll i = 0; i < n; i++) {
+        product *= nums[i];
+        while (product >= k and start < n) product /= nums[start++];
+        subArray += i-start+1;
+    }
+    
+    return subArray;
 }
