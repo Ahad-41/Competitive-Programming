6b0e9ac647fc7d2b16259b2f865a004e661a9d21
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 19:41:08 +0600

    Update Maximum Subarray Sum With Length Between A and B.cpp

--- Algorithms-and-Common-Problems/Maximum Subarray Sum With Length Between A and B.cpp
+++ Algorithms-and-Common-Problems/Maximum Subarray Sum With Length Between A and B.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+typedef long long ll;
  
-int main()
-{
-    int n, a, b; cin >> n >> a >> b;
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll n, a, b; cin >> n >> a >> b;
     
     ll arr[n+1] = {0};
-    for (int i = 1; i <= n; i++) {
+    for (ll i = 1; i <= n; i++) {
         cin >> arr[i];
         arr[i] += arr[i-1];
     }
     
     ll maxSum = -1e18;
     multiset <ll> s;
-
-    for (int i = a; i <= n; i++) {
+    for (ll i = a; i <= n; i++) {
         if (i > b) s.erase(s.find(arr[i-b-1]));
         s.insert(arr[i-a]);
         maxSum = max(maxSum, arr[i] - *s.begin());
     }
     
-    cout << maxSum << '\n';
+    cout << maxSum << "\n";
 }  
