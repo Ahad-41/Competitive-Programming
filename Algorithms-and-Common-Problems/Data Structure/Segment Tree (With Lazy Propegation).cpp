50a2be3a0d17d4af1710d9be8b37ffdb5122b307
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:08:09 +0600

    Update Segment Tree (With Lazy Propegation).cpp

--- Algorithms-and-Common-Problems/Data Structure/Segment Tree (With Lazy Propegation).cpp
+++ Algorithms-and-Common-Problems/Data Structure/Segment Tree (With Lazy Propegation).cpp
@@ -1,46 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-const int N = 1e5+5;
-int arr[N];
+const ll N = 2e5+5;
+ll arr[N];
 
 struct info {
-    long long int prop, sum;
+    long long prop, sum;
 } tree[N*3];
 
 
-// create segment tree ->
-void segmentTree(int currNode, int left, int right) {
+void segmentTree(ll currNode, ll left, ll right) {
     if (left == right) {
         tree[currNode].sum = arr[left];
         return;
     } 
     
-    int leftNode = currNode*2, rightNode = currNode*2 + 1;
-    int mid = left + (right-left)/2;
+    ll leftNode = currNode*2, rightNode = currNode*2 + 1;
+    ll mid = left + (right-left)/2;
     segmentTree(leftNode, left, mid);
     segmentTree(rightNode, mid+1, right);
 
     tree[currNode].sum = tree[leftNode].sum + tree[rightNode].sum;
 }
 
-// return the sum of i-th index to j-th index ->
-int query(int currNode, int left, int right, int i, int j, int carry = 0) {
+ll query(ll currNode, ll left, ll right, ll i, ll j, ll carry = 0) {
     if (i > right || j < left) return 0;
     if (left >= i and right <= j) {
         return tree[currNode].sum + carry * (right - left + 1);
     }
     
-    int leftNode = currNode*2, rightNode = currNode*2 + 1;
-    int mid = left + (right-left)/2;
-    int leftSum = query(leftNode, left, mid, i, j, carry + tree[currNode].prop);
-    int rightSum = query(rightNode, mid + 1, right, i, j, carry + tree[currNode].prop);
+    ll leftNode = currNode*2, rightNode = currNode*2 + 1;
+    ll mid = left + (right-left)/2;
+    ll leftSum = query(leftNode, left, mid, i, j, carry + tree[currNode].prop);
+    ll rightSum = query(rightNode, mid + 1, right, i, j, carry + tree[currNode].prop);
 
-    return leftSum + rightSum;
+    return leftSum+rightSum;
 }
 
-// update in the i-th index to j-th index with new value -> 
-void update(int currNode, int left, int right, int i, int j, int newValue) {
+void update(ll currNode, ll left, ll right, ll i, ll j, ll newValue) {
     if (i > right || j < left) return;
     if (left >= i and right <= j) {
         tree[currNode].sum += ((right - left + 1) * newValue);
@@ -48,18 +46,19 @@ void update(int currNode, int left, int right, int i, int j, int newValue) {
         return;
     }
 
-    int leftNode = currNode*2, rightNode = currNode*2 + 1;
-    int mid = left + (right-left)/2;
+    ll leftNode = currNode*2, rightNode = currNode*2 + 1;
+    ll mid = left + (right-left)/2;
     update(leftNode, left, mid, i, j, newValue);
     update(rightNode, mid+1, right, i, j, newValue);
     
-    tree[currNode].sum = tree[leftNode].sum + tree[rightNode].sum + (right - left + 1) * tree[currNode].prop;
+    tree[currNode].sum = tree[leftNode].sum + tree[rightNode].sum + (right-left+1) * tree[currNode].prop;
 }
 
-int main()
-{
-    int n, q; cin >> n >> q;
-    for (int i = 1; i <= n; i++) cin >> arr[i];
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll n, q; cin >> n >> q;
+
+    for (ll i = 1; i <= n; i++) cin >> arr[i];
     segmentTree(1, 1, n);
 
     while (q--) {
