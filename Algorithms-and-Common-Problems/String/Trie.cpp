b4b3939f1c890b37f7841265d18249139b2cb485
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:43:57 +0600

    Update Trie.cpp

--- Algorithms-and-Common-Problems/String/Trie.cpp
+++ Algorithms-and-Common-Problems/String/Trie.cpp
@@ -4,43 +4,41 @@ typedef long long ll;
 
 struct node {
     bool endmark = false;
-    node *child[52] = {NULL};    
-}; 
+    node *child[52] = {NULL};
+};
 node *rootNode;
 
-ll getIndx(char ch){
+ll getIndx(char ch) {
     ll indx = ch - 'A';
     if (ch >= 'a') indx -= 6;
     return indx;
 }
 
-void insert(string s){
+void insert(string s) {
     node *currNode = rootNode;
     for (auto &u : s) {
         ll indx = getIndx(u);
-        if (currNode -> child[indx] == NULL) {
-            currNode -> child[indx] = new node;
-        }    
-        currNode = currNode -> child[indx];
+        if (currNode->child[indx] == NULL) {
+            currNode->child[indx] = new node;
+        }
+        currNode = currNode->child[indx];
     }
-    currNode -> endmark = true;
+    currNode->endmark = true;
 }
 
-bool search(string s){
+bool search(string s) {
     node *currNode = rootNode;
     for (auto &u : s) {
         ll indx = getIndx(u);
-        if (currNode -> child[indx] == NULL) return false;
-        currNode = currNode -> child[indx];
+        if (currNode->child[indx] == NULL) return false;
+        currNode = currNode->child[indx];
     }
-    return currNode -> endmark;
+    return currNode->endmark;
 }
 
 void Delete(node *currNode) {
     for (ll i = 0; i < 52; i++) {
-        if (currNode -> child[i] != NULL) {
-            Delete(currNode -> child[i]);
-        }
+        if (currNode->child[i] != NULL) Delete(currNode->child[i]);
     }
     delete(currNode);
 }
