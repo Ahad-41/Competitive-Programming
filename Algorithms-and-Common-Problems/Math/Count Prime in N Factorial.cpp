5369336240b93f936464bd393e47a3a27d0fb513
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 17:50:05 +0600

    Update Count Prime in N Factorial.cpp

--- Algorithms-and-Common-Problems/Math/Count Prime in N Factorial.cpp
+++ Algorithms-and-Common-Problems/Math/Count Prime in N Factorial.cpp
@@ -2,7 +2,7 @@ ll countPrimeInFactorial(ll n, ll p) {
     ll count = 0;
     for (ll power = p; power <= n; power *= p) {
         count += n / power;
-        if (power > n / p) break;  // Avoid overflow
+        if (power > n / p) break;
     }
     return count;
 }
