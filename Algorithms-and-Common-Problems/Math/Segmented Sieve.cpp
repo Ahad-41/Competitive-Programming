6afd4753b7bfd8fb461b8f5ad2784c48a1c77908
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:17:14 +0600

    Update Segmented Sieve.cpp

--- Algorithms-and-Common-Problems/Math/Segmented Sieve.cpp
+++ Algorithms-and-Common-Problems/Math/Segmented Sieve.cpp
@@ -1,55 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 32000;
-vector <int> primes;
+typedef long long ll;
+
+const ll N = 2e5+7;
+vector<ll> primes;
 
 void sieve() {
     bool isPrime[N];
     memset(isPrime, true, sizeof(isPrime));
 
-    for (int i = 3; i * i <= N; i += 2) {
+    for (ll i = 3; i*i < N; i += 2) {
         if (isPrime[i]) {
-            for (int j = i*i; j <= N; j += i) isPrime[j] = false;
+            for (ll j = i*i; j <= N; j += i) isPrime[j] = false;
         }   
     } 
 
     primes.push_back(2);
-    for (int i = 3; i <= N; i += 2) {
-        if (isPrime[i]) primes.push_back(i);
-    }
+    for (ll i = 3; i <= N; i += 2) if (isPrime[i]) primes.push_back(i);
 }
 
-void segmentedSieve (int l, int r) {
-    int limit = r-l+1;
+void segmentedSieve (ll l, ll r) {
+    ll limit = r-l+1;
     bool isPrime[limit];
     memset(isPrime, true, sizeof(isPrime));
     
-    for (int i = 0; primes[i] * primes[i] <= r; i++) {
-        int base = (l/primes[i]) * primes[i];
+    for (ll i = 0; primes[i] * primes[i] <= r; i++) {
+        ll base = (l/primes[i]) * primes[i];
         if (base < l) base += primes[i];
 
-        for (int j = base; j <= r; j += primes[i]) {
-            isPrime[j-l] = false;
-        }
-
+        for (ll j = base; j <= r; j += primes[i]) isPrime[j-l] = false;
         if (base == primes[i]) isPrime[base-l] = true;
     }
 
-    for (int i = 0; i < limit; i++) {
-        if (isPrime[i]) cout << l+i << endl;
-    }
-    cout << endl;
+    for (ll i = 0; i < limit; i++) if (isPrime[i]) cout << l+i << "\n";
 }
 
-int main () 
-{
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
     sieve();
-    int t;
-    scanf("%d", &t);
+    int tc; cin >> tc;
 
-    while (t--) {
-        int m, n;
-        scanf("%d %d", &m, &n);
+    test:
+    while (tc--) {
+        ll m, n; cin >> m >> n;
         if (m == 1) m++;
         segmentedSieve(m, n);
     }
