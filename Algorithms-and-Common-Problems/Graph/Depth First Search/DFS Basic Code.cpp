708bbb48942b8e0138aa3b3b6f2b17d84061a234
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 18:04:42 +0600

    Update and rename Basic Code.cpp to DFS Basic Code.cpp

--- Algorithms-and-Common-Problems/Graph/Depth First Search/DFS Basic Code.cpp
+++ Algorithms-and-Common-Problems/Graph/Depth First Search/DFS Basic Code.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+typedef long long ll;
 
 /*----------------------Graph Moves----------------*/
 //const int fx[]={+1,-1,+0,+0};
 //const int fy[]={+0,+0,+1,-1};
-//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
-//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
-//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1}; // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1}; // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2}; // Knights Move
 //const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
 /*------------------------------------------------*/
 
-const ll N = 1e5 + 7;
+const ll N = 2e5+7;
 vector<ll> adjList[N];
 bool visited[N];
 
@@ -19,15 +19,14 @@ void dfs(ll currNode) {
     if (visited[currNode]) return;
     
     visited[currNode] = true;
-    for (auto &u : adjList[currNode]) {
-        dfs(u);
-    }
+    for (auto &u : adjList[currNode]) dfs(u);
 }
 
-int main()
-{
-    ll node, edge; cin >> node >> edge;
-    while (edge--) {
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll n, m; cin >> n >> m;
+
+    while (m--) {
         ll u, v; cin >> u >> v;
         adjList[u].push_back(v);
         adjList[v].push_back(u);
