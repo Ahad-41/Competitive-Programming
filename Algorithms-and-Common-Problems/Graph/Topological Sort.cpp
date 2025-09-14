74065ac2ec56508bab64731d971eabf48148d670
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:39:57 +0600

    Update Topological Sort.cpp

--- Algorithms-and-Common-Problems/Graph/Topological Sort.cpp
+++ Algorithms-and-Common-Problems/Graph/Topological Sort.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-const int N = 1e5 + 7;
-vector<int> adjList[N], topSortArray;
-int inDegree[N], node, edge;
+const ll N = 2e5+7;
+vector<ll> adjList[N], topSortArray;
+ll n, m, inDegree[N];
 
 void topSort() {
-    queue <int> q;
+    queue <ll> q;
+    for (ll i = 1; i <= n; i++) if (inDegree[i] == 0) q.push(i);
     
-    for (int i = 0; i < node; i++) {
-        if (inDegree[i] == 0) q.push(i);
-    }
-
     while (!q.empty()) {
-        int currNode = q.front();
+        ll currNode = q.front();
         q.pop();
         topSortArray.push_back(currNode);
 
@@ -24,17 +22,17 @@ void topSort() {
     }
 }
 
-int main()
-{
-    cin >> node >> edge;
-    while (edge--) {
-        int u, v; cin >> u >> v;
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+
+    while (m--) {
+        ll u, v; cin >> u >> v;
         adjList[u].push_back(v);
         inDegree[v]++;
     }
 
     topSort();
-
     for (auto &u : topSortArray) cout << u << " ";
-    cout << endl;
+    cout << "\n";
 }
