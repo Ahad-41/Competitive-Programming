5e3f6acda1b429384d422d3762bc543d6b3e871d
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-09-14 20:28:12 +0600

    Update Sum of the Divisor of a Number (SOD).cpp

--- Algorithms-and-Common-Problems/Math/Sum of the Divisor of a Number (SOD).cpp
+++ Algorithms-and-Common-Problems/Math/Sum of the Divisor of a Number (SOD).cpp
@@ -2,13 +2,14 @@ vector<ll> prime;
 const ll N = 1e7+7;
 bool check[N];
 void sieve() {
-    for (ll i = 3; i < N; i += 2) 
-        if (!check[i])
+    for (ll i = 3; i*i < N; i += 2) {
+        if (!check[i]) {
             for (ll j = i*i; j < N; j += i) check[j] = true;
+        }
+    } 
 
     prime.push_back(2);
-    for (ll i = 3; i < N; i += 2) 
-        if (!check[i]) prime.push_back(i);
+    for (ll i = 3; i < N; i += 2) if (!check[i]) prime.push_back(i);
 }
 
 ll sumOfDivisor(ll n) {
