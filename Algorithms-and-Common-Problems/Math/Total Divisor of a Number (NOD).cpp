22829047380d8a04096603be9df532cb6c9f5fb5
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-12-15 00:13:09 +0600

    Update Total Divisor of a Number (NOD).cpp

--- Algorithms-and-Common-Problems/Math/Total Divisor of a Number (NOD).cpp
+++ Algorithms-and-Common-Problems/Math/Total Divisor of a Number (NOD).cpp
@@ -1,15 +1,15 @@
-const ll N = 1e6+7;
-bool check[N];
+const ll N = 1e7+5;
+bool isComposite[N];
 vector<ll> prime;
-void sieve() {
-    for (ll i = 3; i*i < N; i += 2) {
-        if (!check[i]) {
-            for (ll j = i*i; j < N; j += i) check[j] = true;
-        }
-    }
 
-    prime.push_back(2);
-    for (ll i = 3; i < N; i += 2) if (!check[i]) prime.push_back(i);
+void sieve() {
+	for (ll i = 2; i < N; i++) {
+		if (!isComposite[i]) prime.push_back(i);
+		for (ll j = 0; j < prime.size() and i * prime[j] < N; j++) {
+			isComposite[i * prime[j]] = true;
+			if (i % prime[j] == 0) break;
+		}
+	}
 }
 
 ll divisorCount(ll n) {
