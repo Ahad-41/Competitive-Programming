035a4fc7526cabbad25dace9e5275b699f1c37b1
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-12-14 23:29:14 +0600

    Update DFS Basic Code.cpp

--- Algorithms-and-Common-Problems/Graph/Depth First Search/DFS Basic Code.cpp
+++ Algorithms-and-Common-Problems/Graph/Depth First Search/DFS Basic Code.cpp
@@ -2,33 +2,44 @@
 using namespace std;
 typedef long long ll;
 
-/*----------------------Graph Moves----------------*/
-//const int fx[]={+1,-1,+0,+0};
-//const int fy[]={+0,+0,+1,-1};
-//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1}; // Kings Move
-//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1}; // Kings Move
-//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2}; // Knights Move
-//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
-/*------------------------------------------------*/
-
 const ll N = 2e5+7;
 vector<ll> adjList[N];
-bool visited[N];
+ll arr[N];
 
-void dfs(ll currNode) {
-    if (visited[currNode]) return;
-    
-    visited[currNode] = true;
-    for (auto &u : adjList[currNode]) dfs(u);
+/*---------------------- Graph Moves ----------------*/
+//const int dx[]={+1,-1,+0,+0};
+//const int dy[]={+0,+0,+1,-1};
+//const int dx[]={+0,+0,+1,-1,-1,+1,-1,+1}; // Kings Move
+//const int dy[]={-1,+1,+0,+0,+1,+1,-1,-1}; // Kings Move
+//const int dx[]={-2, -2, -1, -1,  1,  1,  2,  2}; // Knights Move
+//const int dy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+void dfs(ll currNode, ll parent = -1) {
+    for (auto &u : adjList[currNode]) {
+        if (u == parent) continue;
+        dfs(u, currNode);
+    }
 }
 
 signed main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    ll n, m; cin >> n >> m;
+    int tc; cin >> tc;
 
-    while (m--) {
-        ll u, v; cin >> u >> v;
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
+    test:
+    while (tc--) {
+        ll n; cin >> n;
+        for (ll i = 1; i <= n; i++) {
+            adjList[i].clear();
+            cin >> arr[i];
+        }
+
+        for (ll i = 1; i < n; i++) {
+            ll u, v; cin >> u >> v;
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+
+        
     }
 }
