7fae2bc123105717e1a8d169a8b41736a81fbf75
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2025-12-14 23:18:41 +0600

    Delete Algorithms-and-Common-Problems/Dynamic Programming/Coin Change 1.cpp

--- Algorithms-and-Common-Problems/Dynamic Programming/Coin Change 1.cpp
+++
@@ -1,9 +0,0 @@
-ll func(ll amount) {
-    if (amount == 0) return 0;
-    if (dp[amount] != -1) return dp[amount];
-
-    ll ans = 1e18;
-    for (auto &u : coins) if (amount - u >= 0) ans = min(ans, func(amount-u)+1);
-
-    return dp[amount] = ans; 
-}
